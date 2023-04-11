c4f8b1c9c9a75a46c7e8791c41e681c5cdac4907
juampa95 <jpmanzano95@gmail.com>
2023-04-10 22:43:50 -0300

    Avance práctico 5

--- Modulo3/Practico5/Practico5.py
+++ Modulo3/Practico5/Practico5.py
@@ -5,6 +5,8 @@ Created on 10/04/2023
 @author: Juan Pablo Manzano
 """
 import numpy as np
+import pandas as pd
+import os
 
 # Practico 5
 
@@ -21,26 +23,78 @@ y_prom = np.mean(y)
 m = 0
 difx = 0
 for j in range(len(x)):
-    difx = difx + (x[j]-x_prom)**2
+    difx = difx + (x[j] - x_prom) ** 2
 for i in range(len(x)):
-    m = m + ((x[i] - x_prom)*(y[i] - y_prom))/(difx)
+    m = m + ((x[i] - x_prom) * (y[i] - y_prom)) / (difx)
 
 b = y_prom - m * x_prom
 
-print(f'la recta de ajuste es {round(m,2)}x + ({round(b,2)})')
+print(f'la recta de ajuste es {round(m, 2)}x + ({round(b, 2)})')
 
 y_pred = []
 for i in range(len(x)):
-    yp = m*x[i]+b
+    yp = m * x[i] + b
     y_pred.append(yp)
 
 y_pred
 sum = 0
 for i in range(len(x)):
-    sum = sum + ((y[i]-y_pred[i])**2)
-j = sum/(2*len(x))
+    sum = sum + ((y[i] - y_pred[i]) ** 2)
+j = sum / (2 * len(x))
 
 j
 
-print(f'la funcion costo me da {round(j,2)}. Es distinto al valor 18,95 del practico \n'
+print(f'la funcion costo me da {round(j, 2)}. Es distinto al valor 18,95 del practico \n'
       f'creo que es porque en el PDF se olvidaron de dividirlo por 2N')
+
+# Creamos una funcion que me determine 'm','b'y el valor de la funcion de costo
+def reg_lin_una_var(xs, ys):
+    x_prom = np.mean(xs)
+    y_prom = np.mean(ys)
+    m = 0
+    difx = 0
+    for j in range(len(xs)):
+        difx = difx + (xs[j] - x_prom) ** 2
+    for i in range(len(xs)):
+        m = m + ((xs[i] - x_prom) * (ys[i] - y_prom)) / (difx)
+    b = y_prom - m * x_prom
+    sum = 0
+    for i in range(len(xs)):
+        sum = sum + ((ys[i] - (m * xs[i] + b)) ** 2)
+    j = sum / (2 * len(xs))
+    return [m, b, j]
+
+resultado = reg_lin_una_var(x,y)
+print(f'la recta de ajuste tiene un m = {resultado[0]}\n'
+      f'la recta de ajuste tiene un b = {resultado[1]}\n'
+      f'la funcion de costo tiene un valor de j = {resultado[2]}')
+
+
+
+# =============================================================================
+# Ejercicio 2
+# =============================================================================
+print(os.getcwd())
+os.chdir('D:\gitProyects\maestria-ML-UTN\Modulo3\Practico5')
+
+datos = pd.read_csv('heights_weights.csv')
+
+datos
+
+x_2 = datos['Height'].tolist()
+y_2 = datos['Weight'].tolist()
+
+resultado_2 = reg_lin_una_var(x_2,y_2)
+
+print(f'la recta de ajuste tiene un m = {resultado_2[0]}\n'
+      f'la recta de ajuste tiene un b = {resultado_2[1]}\n'
+      f'la funcion de costo tiene un valor de j = {resultado_2[2]}')
+
+print(f'estimación del peso de una mujer de 1.3m = {resultado_2[0]*1.3+resultado_2[1]}')
+print(f'estimación del peso de una mujer de 1.6m = {resultado_2[0]*1.6+resultado_2[1]}')
+print(f'estimación del peso de una mujer de 2.0m = {resultado_2[0]*2.0+resultado_2[1]}')
+
+# =============================================================================
+# Ejercicio 3
+# =============================================================================
+
