c9aad4e09e55229f020f43ad10c5234499fcc64b
juampa95 <jpmanzano95@gmail.com>
2023-04-11 20:22:18 -0300

    Avances, resolucion parcial ejercicio 4

--- Modulo3/Practico5/Practico5.py
+++ Modulo3/Practico5/Practico5.py
@@ -7,6 +7,7 @@ Created on 10/04/2023
 import numpy as np
 import pandas as pd
 import os
+os.chdir('D:\gitProyects\maestria-ML-UTN\Modulo3\Practico5')
 
 # Practico 5
 
@@ -75,7 +76,6 @@ print(f'la recta de ajuste tiene un m = {resultado[0]}\n'
 # Ejercicio 2
 # =============================================================================
 print(os.getcwd())
-os.chdir('D:\gitProyects\maestria-ML-UTN\Modulo3\Practico5')
 
 datos = pd.read_csv('heights_weights.csv')
 
@@ -98,3 +98,61 @@ print(f'estimación del peso de una mujer de 2.0m = {resultado_2[0]*2.0+resultad
 # Ejercicio 3
 # =============================================================================
 
+print(os.getcwd())
+
+datos = pd.read_csv('notas_horasestudio.csv')
+
+datos
+
+x_3 = datos['Horas'].tolist()
+y_3 = datos['Notas'].tolist()
+
+resultado_3 = reg_lin_una_var(x_3,y_3)
+
+resultado_3
+print(f'la recta de ajuste tiene un m = {resultado_3[0]}\n'
+      f'la recta de ajuste tiene un b = {resultado_3[1]}\n'
+      f'la funcion de costo tiene un valor de j = {resultado_3[2]}')
+
+# =============================================================================
+# Ejercicio 4
+# =============================================================================
+
+x_4 = [4.1,6,6,7.3,7.8,8.1,8.77,9.45,12.3,14.2,15,16,18.12]
+y_4 = [90,89,83,81,73,71,68,60,59,54,35,35,21]
+
+# MODO DIRECTO CON FUNCION CREADA
+resultado_4_directo = reg_lin_una_var(x_4,y_4)
+print(f'la recta de ajuste tiene un m = {resultado_4_directo[0]}\n'
+      f'la recta de ajuste tiene un b = {resultado_4_directo[1]}\n'
+      f'la funcion de costo tiene un valor de j = {resultado_4_directo[2]}')
+
+# MODO POR FUERZA BRUTA
+i= 0
+resultados_4_fbruta = pd.DataFrame(columns=['m','b','j'])
+for m in range(-10,0):
+    for b in range(100,200):
+        sum = 0
+        for x in range(len(x_4)):
+            ypred = m*x_4[x]+b
+            sum = sum + ((y_4[x] - (m * x_4[x] + b)) ** 2)
+        j = sum / (2 * len(x_4))
+        resultados_4_fbruta.loc[i] = {'m': m, 'b': b, 'j': j}
+        i += 1
+
+r4_fb = resultados_4_fbruta.sort_values('j',ascending=True).head(1)
+
+print(f'la recta de mejor ajuste sera la que tenga los siguientes parametros\n'
+      f'y su funcion de costo valdra lo que marca la tabla en la columna j\n'
+      f'{r4_fb}')
+
+# MODO DESCENSO POR GRADIENTE
+
+# concepto de derivada, cuanto crece la funcion, cuando el incremento tiende a 0
+# la funcion incrementada, menos la funcion sin incrementar sobre el incremento
+
+
+
+# =============================================================================
+# Ejercicio 5
+# =============================================================================
